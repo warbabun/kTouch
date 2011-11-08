6ac6369d611e3f7822a5ee3da71dd7bc8beb579a
warbabun <mikhail.gorodilin@gmail.com>
2011-11-08 01:45:41 +0100

    Finished creating XDocument from an arbitrary directory. Began merging UniversCreator and kTouch.

--- KTouch.Units/ItemsLoader.cs
+++ KTouch.Units/ItemsLoader.cs
@@ -18,8 +18,8 @@ namespace KTouch.Units {
 
         public static Dictionary<string, ObservableCollection<KTouchItem>> InputCollections = null;
 
-        public delegate void EventHandler ( KTouchItem obj );
-        public delegate void PackEventHandler ( List<KTouchItem> pack );
+        public delegate void EventHandler(KTouchItem obj);
+        public delegate void PackEventHandler(List<KTouchItem> pack);
 
         public static event EventHandler FileArrived;
         public static event PackEventHandler FilePackArrived;
@@ -28,10 +28,10 @@ namespace KTouch.Units {
         /// Analyse des fichiers de déclaration.
         /// </summary>
         /// <returns>L'ensemble des éléments déclarés.</returns>
-        public static IEnumerable<KTouchItem> LoadItems ( string path ) {
-            List<KTouchItem> items = new List<KTouchItem> ( );
-            foreach ( string xmlFile in Directory.GetFiles ( path, "*.ktouch.xml", SearchOption.AllDirectories ) ) {
-                items.AddRange ( LoadXmlDeclarationFile ( xmlFile ) );
+        public static IEnumerable<KTouchItem> LoadItems(string path) {
+            List<KTouchItem> items = new List<KTouchItem>();
+            foreach(string xmlFile in Directory.GetFiles(path, "*.ktouch.xml", SearchOption.AllDirectories)) {
+                items.AddRange(LoadXmlDeclarationFile(xmlFile));
             }
             return items;
         }
@@ -41,83 +41,82 @@ namespace KTouch.Units {
         /// </summary>
         /// <param name="fileName">Chemin du fichier XML de déclaration.</param>
         /// <returns>Liste des éléments déclarés.</returns>
-        public static List<KTouchItem> LoadXmlDeclarationFile ( string fileName ) {
+        public static List<KTouchItem> LoadXmlDeclarationFile(string fileName) {
 
-            List<KTouchItem> items = new List<KTouchItem> ( );
-            XmlDocument document = new XmlDocument ( );
-            document.Load ( fileName );
+            List<KTouchItem> items = new List<KTouchItem>();
+            XmlDocument document = new XmlDocument();
+            document.Load(fileName);
 
             XmlNode root = document.DocumentElement;
-            foreach ( XmlNode node in root.ChildNodes ) {
-                string directory = ( Directory.GetParent ( fileName ) ).FullName + @"\" + node.Attributes [ "Directory" ].Value;
+            foreach(XmlNode node in root.ChildNodes) {
+                string directory = (Directory.GetParent(fileName)).FullName + @"\" + node.Attributes["Directory"].Value;
                 KTouchItem item = new KTouchItem {
                     Directory = directory,
-                    CoverFile = directory + @"\" + node.Attributes [ "CoverFile" ].Value,
-                    File = directory + @"\" + node.Attributes [ "File" ].Value,
-                    Title = node.Attributes [ "Title" ].Value,
-                    Type = node.Attributes [ "Type" ].Value
+                    CoverFile = directory + @"\" + node.Attributes["CoverFile"].Value,
+                    File = directory + @"\" + node.Attributes["File"].Value,
+                    Title = node.Attributes["Title"].Value,
+                    Type = node.Attributes["Type"].Value
                 };
-                items.Add ( item );
+                items.Add(item);
             }
             return items;
         }
 
-        public static List<KTouchUniverse> LoadUniverses ( string fileName ) {
-            XmlDocument doc = new XmlDocument ( );
-            doc.Load ( fileName );
+        public static List<KTouchUniverse> LoadUniverses(string fileName) {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(fileName);
 
-            List<KTouchUniverse> universes = new List<KTouchUniverse> ( );
+            List<KTouchUniverse> universes = new List<KTouchUniverse>();
 
             XmlNode root = doc.DocumentElement;
-            foreach ( XmlNode node in root.ChildNodes ) {
-                string directory = ( Directory.GetParent ( fileName ) ).FullName + @"\" + node.Attributes [ "Directory" ].Value;
+            foreach(XmlNode node in root.ChildNodes) {
+                string directory = (Directory.GetParent(fileName)).FullName + @"\" + node.Attributes["Directory"].Value;
                 KTouchUniverse universe = new KTouchUniverse {
                     Directory = directory,
-                    File = directory + @"\" + node.Attributes [ "File" ].Value,
-                    Name = node.Attributes [ "Name" ].Value,
+                    File = directory + @"\" + node.Attributes["File"].Value,
+                    Name = node.Attributes["Name"].Value,
                 };
-                universes.Add ( universe );
+                universes.Add(universe);
             }
             return universes;
         }
 
-        public static void LoadFileAsync ( string fileName ) {
-            LoadFileAsync ( fileName, 9 );
+        public static void LoadFileAsync(string fileName) {
+            LoadFileAsync(fileName, 9);
         }
 
+        public static void LoadFileAsync(string fileName, int packcount) {
 
-        public static void LoadFileAsync ( string fileName, int packcount ) {
-
-            XmlDocument document = new XmlDocument ( );
-            document.Load ( fileName );
+            XmlDocument document = new XmlDocument();
+            document.Load(fileName);
 
             XmlNode root = document.DocumentElement;
-            var tempList = new List<KTouchItem> ( );
-            foreach ( XmlNode node in root.ChildNodes ) {
-                string directory = ( Directory.GetParent ( fileName ) ).FullName + @"\" + node.Attributes [ "Directory" ].Value;
+            var tempList = new List<KTouchItem>();
+            foreach(XmlNode node in root.ChildNodes) {
+                string directory = (Directory.GetParent(fileName)).FullName + @"\" + node.Attributes["Directory"].Value;
                 KTouchItem item = new KTouchItem {
                     Directory = directory,
-                    CoverFile = directory + @"\" + node.Attributes [ "CoverFile" ].Value,
-                    File = directory + @"\" + node.Attributes [ "File" ].Value,
-                    Title = node.Attributes [ "Title" ].Value,
-                    Type = node.Attributes [ "Type" ].Value
+                    CoverFile = directory + @"\" + node.Attributes["CoverFile"].Value,
+                    File = directory + @"\" + node.Attributes["File"].Value,
+                    Title = node.Attributes["Title"].Value,
+                    Type = node.Attributes["Type"].Value
                 };
-                if ( tempList.Count < packcount && FilePackArrived != null )
-                    tempList.Add ( item );
-                else if ( FilePackArrived != null )
-                    FilePackArrived ( tempList );
-                else if ( FileArrived != null )
-                    FileArrived ( item );
+                if(tempList.Count < packcount && FilePackArrived != null)
+                    tempList.Add(item);
+                else if(FilePackArrived != null)
+                    FilePackArrived(tempList);
+                else if(FileArrived != null)
+                    FileArrived(item);
             }
         }
 
-        public static void LoadCollections ( string path ) {
+        public static void LoadCollections(string path) {
             _currentDispatcher = Dispatcher.CurrentDispatcher;
-            InputCollections = new Dictionary<string, ObservableCollection<KTouchItem>> ( );
-            foreach ( var universe in ItemsLoader.LoadUniverses ( path ) ) {
-                List<KTouchItem> items = ItemsLoader.LoadXmlDeclarationFile ( universe.File );
-                if ( items != null ) {
-                    InputCollections [ universe.Name ] = new ObservableCollection<KTouchItem> ( items );
+            InputCollections = new Dictionary<string, ObservableCollection<KTouchItem>>();
+            foreach(var universe in ItemsLoader.LoadUniverses(path)) {
+                List<KTouchItem> items = ItemsLoader.LoadXmlDeclarationFile(universe.File);
+                if(items != null) {
+                    InputCollections[universe.Name] = new ObservableCollection<KTouchItem>(items);
                 }
             }
         }
@@ -125,48 +124,44 @@ namespace KTouch.Units {
     public class ResultEventArgs : EventArgs {
         public List<KTouchItem> Items { get; set; }
 
-        public ResultEventArgs ( IEnumerable<KTouchItem> items ) {
-            this.Items = new List<KTouchItem> ( items );
+        public ResultEventArgs(IEnumerable<KTouchItem> items) {
+            this.Items = new List<KTouchItem>(items);
         }
     }
 
 
     public class XMLParser {
-        public delegate void EventHandler ( object sender, ResultEventArgs e );
+        public delegate void EventHandler(object sender, ResultEventArgs e);
 
         public event EventHandler Completed;
 
         string directory;
 
-        public void LoadXMLFile ( string xmlURI ) {
-            WebClient xmlClient = new WebClient ( );
-            directory = ( Directory.GetParent ( xmlURI ) ).FullName + @"\";
-            xmlClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler ( XMLFileLoaded );
-            xmlClient.DownloadStringAsync ( new Uri ( xmlURI, UriKind.RelativeOrAbsolute ) );
+        public void LoadXMLFile(string xmlURI) {
+            WebClient xmlClient = new WebClient();
+            directory = (Directory.GetParent(xmlURI)).FullName + @"\";
+            xmlClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(XMLFileLoaded);
+            xmlClient.DownloadStringAsync(new Uri(xmlURI, UriKind.RelativeOrAbsolute));
         }
 
-        public void XMLFileLoaded ( object sender, DownloadStringCompletedEventArgs e ) {
-            if ( e.Error == null ) {
+        public void XMLFileLoaded(object sender, DownloadStringCompletedEventArgs e) {
+            if(e.Error == null) {
                 string xmlData = e.Result;
-                if ( xmlData != null ) {
-                    XDocument xdoc = XDocument.Parse ( xmlData );
-                    var items = from item in xdoc.Descendants ( "KTouchItem" )
+                if(xmlData != null) {
+                    XDocument xdoc = XDocument.Parse(xmlData);
+                    var items = from item in xdoc.Descendants("KTouchItem")
                                 select new KTouchItem {
-                                    Directory = directory + item.Attribute ( "Directory" ).Value,
-                                    CoverFile = directory + item.Attribute ( "Directory" ).Value + @"/" + item.Attribute ( "CoverFile" ).Value,
-                                    File = directory + item.Attribute ( "Directory" ).Value + @"/" + item.Attribute ( "File" ).Value,
-                                    Title = item.Attribute ( "Title" ).Value,
-                                    Type = item.Attribute ( "Type" ).Value,
+                                    Directory = directory + item.Attribute("Directory").Value,
+                                    CoverFile = directory + item.Attribute("Directory").Value + @"/" + item.Attribute("CoverFile").Value,
+                                    File = directory + item.Attribute("Directory").Value + @"/" + item.Attribute("File").Value,
+                                    Title = item.Attribute("Title").Value,
+                                    Type = item.Attribute("Type").Value,
                                 };
-                    if ( Completed != null ) {
-                        Completed ( this, new ResultEventArgs ( items ) );
+                    if(Completed != null) {
+                        Completed(this, new ResultEventArgs(items));
                     }
                 }
             }
         }
-
-
-
-
     }
 }
---
+++ KTouch.Units/SupportedExtenstions.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+namespace KTouch.Units {
+    public static class SupportedExtensions {
+
+        /// <summary>
+        /// Xps file.
+        /// </summary>
+        public const string XPS = ".xps";
+
+        /// <summary>
+        /// Wmv file.
+        /// </summary>
+        public const string WMV = ".wmv";
+
+        /// <summary>
+        /// Mp4 file.
+        /// </summary>
+        public const string MP4 = ".mp4";
+
+        /// <summary>
+        /// Mpg file.
+        /// </summary>
+        public const string MPG = ".mpg";
+
+        /// <summary>
+        /// Avi file.
+        /// </summary>
+        public const string AVI = ".avi";
+
+        /// <summary>
+        /// Jpg file.
+        /// </summary>
+        public const string JPG = ".jpg";
+
+        /// <summary>
+        /// Png file.
+        /// </summary>
+        public const string PNG = ".png";
+
+        /// <summary>
+        /// Gif file.
+        /// </summary>
+        public const string GIF = ".gif";
+
+        private List<string> _supportedExtensionList;
+        private List<string> _supportedDocumentExtensionList;
+        private List<string> _supportedMediaExtensionList;
+        private List<string> _supportedThumbnailExtensionList;
+
+        /// <summary>
+        /// Returns the list of supported file extensions.
+        /// </summary>
+        private ICollection<string> SupportedExtensionList {
+            get {
+                if(_supportedExtensionList == null) {
+                    _supportedExtensionList = new List<string>();
+                    _supportedExtensionList.AddRange(SupportedDocumentExtensionList);
+                    _supportedExtensionList.AddRange(SupportedMediaExtensionList);
+                }
+                return _supportedExtensionList;
+            }
+        }
+
+        /// <summary>
+        /// Returns the list of supported document file extensions.
+        /// </summary>
+        private ICollection<string> SupportedDocumentExtensionList {
+            get {
+                if(_supportedDocumentExtensionList == null) {
+                    _supportedDocumentExtensionList = new List<string> {
+                        SupportedExtensions.XPS,                
+                    };
+                }
+                return _supportedDocumentExtensionList;
+            }
+        }
+
+        /// <summary>
+        /// Returns the list of supported media file extensions.
+        /// </summary>
+        private ICollection<string> SupportedMediaExtensionList {
+            get {
+                if(_supportedMediaExtensionList == null) {
+                    _supportedMediaExtensionList = new List<string>{
+                        SupportedExtensions.AVI,
+                        SupportedExtensions.MP4,
+                        SupportedExtensions.MPG,
+                        SupportedExtensions.WMV,
+                    };
+                }
+                return _supportedMediaExtensionList;
+            }
+        }
+
+        /// <summary>
+        /// Returns the list of output thumbnail image extensions.
+        /// </summary>
+        private ICollection<string> SupportedThumbnailExtensionList {
+            get {
+                if(_supportedThumbnailExtensionList == null) {
+                    _supportedThumbnailExtensionList = new List<string> {
+                        SupportedExtensions.GIF,
+                        SupportedExtensions.JPG,
+                        SupportedExtensions.PNG,
+                    };
+                }
+                return _supportedThumbnailExtensionList;
+            }
+        }
+    }
+}
